b8ba1ef0f24e4fc84232505b48cb468b3fc299c1
aporporo <117672857+aporporo@users.noreply.github.com>
2024-05-24 17:50:20 +0000

    defined database details

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST',        'dv8.h.filess.io');
+define('DB_USER',        'COSC4806_successful');
+define('DB_PASS',        $_ENV['DB_PASS']);
+define('DB_DATABASE',    'COSC4806_successful');
+define('DB_PORT',        '3305');
+
+?>
