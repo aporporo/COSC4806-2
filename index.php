526dbcf7b3dc96de5e38c877f593e881161d761a
aporporo <117672857+aporporo@users.noreply.github.com>
2024-05-28 15:31:51 +0000

    update to index text

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ if (isset($_SESSION['authenticated']) != 1) {
 
 <html>
   <head>
-    <title>COSC4806 Assignment 1 - Alex</title>
+    <title>COSC4806 Assignment 2 - Alex</title>
   </head>
   <body>
 
-    <h1>Assignment 1</h1>
+    <h1>Assignment 2</h1>
 
     <p> Welcome, <?=$_SESSION['username'] ?>!</p>
     <?php echo "Today is " . date("Y/m/d") . "<br>"; ?>
